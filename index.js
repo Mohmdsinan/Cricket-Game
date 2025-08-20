a96e657632e2b46dd8c7d19cd7033d5285d55aab
Mohmdsinan <sinankdty@gmail.com>
2025-08-20 08:30:16 +0530

    updated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-function tossSelection(choice){
-  const tossResult = Math.random() < 0.5 ? 'head' : 'tail' ;
-  
+function tossSelection(choice) {
+  const tossResult = Math.random() < 0.5 ? 'head' : 'tail';
+
 
   if (tossResult === choice) {
     const pTag = document.querySelector('.bat-ball');
@@ -12,10 +12,13 @@ function tossSelection(choice){
 
     const batButton = document.createElement("button");
     batButton.innerText = "Bat";
+    batButton.addEventListener("click", () => selected('bat'));
     batButton.classList.add("choose-button");
 
     const ballButton = document.createElement("button");
     ballButton.innerText = "Ball";
+    ballButton.addEventListener("click", () =>  selected('ball'));
+
     batButton.classList.add("choose-button");
 
     pTag.appendChild(batButton);
@@ -25,4 +28,30 @@ function tossSelection(choice){
     const pTag = document.querySelector('.bat-ball');
     pTag.innerHTML = 'You lose in toss';
   }
+}
+
+function selected(choice) {
+  if (choice === 'bat') {
+    
+    const gameArea = document.querySelector(".game-area");
+
+    gameArea.innerText ="Your batting Turn! \n choose your run \n";
+
+
+    for(let i= 1; i<= 6; i++){
+      if(i === 5)
+        continue;
+
+      else{
+        runButton = document.createElement("button");
+        runButton.innerText = i;
+        gameArea.appendChild(runButton);
+        runButton.addEventListener ("click", ()=> userRuns (i)) ;
+      }
+    }
+  }
+}
+
+function userRuns (runs){
+  
 }
